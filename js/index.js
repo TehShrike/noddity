afa5d8c45b325ec93f95a43f5367356b08962936
Josh Duff <me@JoshDuff.com>
2014-07-26 11:50:52 -0500

    Now using per-site title sublevels

--- js/index.js
+++ js/index.js
@@ -5,12 +5,17 @@ var Linkifier = require('noddity-linkifier')
 var Leveldown = require('localstorage-down')
 var routing = require('./routing')
 var Model = require('./mainViewModel')
+var Sublevel = require('level-sublevel')
 var config = noddityConfig
 
 // Safari doesn't support indexedDB; have to use localstorage in that case
 var storage = window.indexedDB ? leveljs : function leveldownFactory(location) { return new Leveldown(location) }
+var db = Sublevel(levelup('noddity-content', { db: storage }))
+var normalizedSublevelName = config.title.replace(/[^\w]+/g, '')
+
 var butlerOptions = config.debug ? { refreshEvery: 30 * 1000 } : undefined
-var butler = new Butler(config.noddityRoot, levelup('content', { db: storage }), butlerOptions)
+var butler = new Butler(config.noddityRoot, db.sublevel(normalizedSublevelName), butlerOptions)
+
 var linkifyEmitter = new Linkifier(config.pathPrefix + config.pagePathPrefix)
 
 var model = new Model(butler, linkifyEmitter)
