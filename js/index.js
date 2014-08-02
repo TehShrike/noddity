72134b7238561d45c3662b53675fd42022bcfd4f
Josh Duff <me@JoshDuff.com>
2014-08-02 22:26:05 +0000

    2.0.4: Using localstorage on IE11

--- js/index.js
+++ js/index.js
@@ -6,12 +6,24 @@ var Leveldown = require('localstorage-down')
 var routing = require('./routing')
 var Model = require('./mainViewModel')
 var Sublevel = require('level-sublevel')
+var browser = require('bowser').browser
 var config = noddityConfig
 
-// Safari doesn't support indexedDB; have to use localstorage in that case
-var storage = window.indexedDB ? leveljs : function leveldownFactory(location) { return new Leveldown(location) }
+var ie11 = browser.msie && browser.version === '11.0'
+
+// IE11 isn't currently supported by the IDBWrapper module used by level-js
+var storage
+if (window.indexedDB && !ie11) {
+	storage = leveljs
+	console.log("Storing Noddity content in an IndexedDB")
+} else {
+	storage = function leveldownFactory(location) { return new Leveldown(location) }
+	console.log("Storing Noddity content in localstorage")
+}
+
 var db = Sublevel(levelup('noddity-content', { db: storage }))
 var normalizedSublevelName = config.title.replace(/[^\w]+/g, '')
+
 
 var butlerOptions = config.debug ? { refreshEvery: 30 * 1000 } : undefined
 var butler = new Butler(config.noddityRoot, db.sublevel(normalizedSublevelName), butlerOptions)
