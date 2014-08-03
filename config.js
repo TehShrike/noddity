2d4e0969da77d13bd5d90a3fea75aae61269c193
Josh Duff <me@JoshDuff.com>
2014-08-03 16:58:23 -0500

    Building for gh-pages

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 noddityConfig = {
 	title: 'Noddity',
 	noddityRoot: window.location.pathname + 'content/',
-	editLink: 'https://github.com/TehShrike/noddity/edit/master/content/',
+	editLink: 'https://github.com/TehShrike/noddity/edit/gh-pages/content/',
 	logo: 'logo.svg',
 	errorPage: '404.md',
 	pathPrefix: '#!/',
