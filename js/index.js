9543b935ab19454fea848a66bc1148becfebdd80
Josh Duff <me@JoshDuff.com>
2015-05-30 16:50:15 -0500

    Ractive debug mode is tied to Noddity debug mode

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ var storage = function leveldownFactory(location) { return new Leveldown(locatio
 var db = levelup('noddity-content-3', { db: storage })
 var normalizedSublevelName = config.title.replace(/[^\w]+/g, '')
 
-
+require('ractive').DEBUG = config.debug
 var butlerOptions = config.debug ? { refreshEvery: 30 * 1000 } : { cacheCheckIntervalMs: 60 * 1000 }
 var butler = new Butler(config.noddityRoot, sub(db, normalizedSublevelName), butlerOptions)
 
